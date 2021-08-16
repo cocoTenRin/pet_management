bd490b0fa2858895060810a47be9143c6b368711
cocoTenRin <jiduck1@gmail.com>
2021-08-16 16:33:59 +0900

    add screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:pet_magament/screen/petList.dart';
+
+import 'Widget/bottom_bar.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  static const String _title = 'Flutter Code Sample';
+class MyApp extends StatefulWidget {
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'chat used firebase',
-      theme: ThemeData(primaryColor: Colors.white),
+      title: 'Pet',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.black,
+        accentColor: Colors.white,
+      ),
       home: DefaultTabController(
-          length: 4,
-          child: Scaffold(
-            appBar: AppBar(
-              title: Text('ペット管理'),
-            ),
-            body: TabBarView(
-              children: [
-                Text('홈 스크린'),
-                Text('채팅 스크린'),
-                Text('마이 스크린'),
-                Text('ㅇㅇ')
-              ],
-            ),
-            bottomNavigationBar: TabBar(tabs: [
-              Tab(
-                icon: Icon(Icons.app_registration),
-                text: '記録',
-              ),
-              Tab(
-                icon: Icon(Icons.calendar_view_week_sharp),
-                text: 'カレンダー',
-              ),
-              Tab(
-                icon: Icon(Icons.list),
-                text: 'ペット一覧',
-              ),
-              Tab(
-                icon: Icon(Icons.app_registration_sharp),
-                text: '登録',
-              )
-            ]),
-          )),
+        length: 4,
+        child: Scaffold(
+          body: TabBarView(
+            physics: NeverScrollableScrollPhysics(),
+            children: <Widget>[
+              PetList(),
+              Container(child: Center(child: Text('홈')),),
+              Container(child: Center(child: Text('홈')),),
+              Container(child: Center(child: Text('홈')),),
+            ],
+          ),
+          bottomNavigationBar: Bottom(),
+        ),
+      ),
     );
   }
 }
---
+++ lib/screen/petList.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class PetList extends StatefulWidget {
+  _PetList createState() => _PetList();
+}
+
+class _PetList extends State<PetList> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Topbar();
+  }
+}
+
+class Topbar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(20, 7, 20, 7),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Image.asset(
+            'imgs/IMG_20150111_135850.JPG',
+            fit: BoxFit.contain,
+            height: 25,
+          ),
+          Container(
+              padding: EdgeInsets.only(right: 1),
+              child: Text(
+                '펫 일람',
+                style: TextStyle(fontSize: 14),
+              ),
+          ),
+        ],
+      ),
+    );
+  }
+}
